6865d7fa8e4508570f680175e741d95407d2210d
wagezhang <30321897+wagezhang@users.noreply.github.com>
2018-01-01 14:31:51 -0500

    Create insertdata.php

---
+++ insertdata.php
@@ -0,0 +1,23 @@
+
+<?php
+
+if( isset( $_POST['user_name'] ) )
+{
+
+$name = $_POST['user_name'];
+$age = $_POST['user_age'];
+$course = $_POST['user_course'];
+
+$host = 'localhost';
+$user = 'root';
+$pass = ' ';
+
+mysql_connect($host, $user, $pass);
+
+mysql_select_db('demo');
+
+$insertdata=" INSERT INTO user_info VALUES( '$name','$age','$course' ) ";
+mysql_query($insertdata);
+
+}
+?>
